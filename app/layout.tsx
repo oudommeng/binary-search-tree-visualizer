d2eac1785ee5acb53708dd2c4ff244f8207b7781
Oudom Meng <Oudombig007@gmail.com>
2025-12-29 14:58:19 +0700

    feat: update metadata with team authors and keywords; add Open Graph and Twitter card data

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,28 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'BST Visualizer - CADT',
-  description: 'Interactive Binary Search Tree Visualizer - CADT',
+  title: 'BST Visualizer',
+  description: 'Interactive Binary Search Tree Visualizer by Team 01 - Pun Solita, Meng Oudom, Khoun Sovansunchhay',
+  authors: [
+    { name: 'Pun Solita' },
+    { name: 'Meng Oudom' },
+    { name: 'Khoun Sovansunchhay' }
+  ],
+  keywords: ['Binary Search Tree', 'BST', 'Visualizer', 'CADT', 'Data Structures', 'Algorithm'],
   icons: {
     icon: '/icon.png',
+  },
+  openGraph: {
+    title: 'BST Visualizer',
+    description: 'Interactive Binary Search Tree Visualizer by Team 01 - Pun Solita, Meng Oudom, Khoun Sovansunchhay',
+    images: ['/bst_sso.png'],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'BST Visualizer',
+    description: 'Interactive Binary Search Tree Visualizer by Team 01 - Pun Solita, Meng Oudom, Khoun Sovansunchhay',
+    images: ['/bst_sso.png'],
   },
 }
 
